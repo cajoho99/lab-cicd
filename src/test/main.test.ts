00700342895a28592470a47080b36d29c969d971
carl.holmberg <carl.holmberg@lynkco.com>
2025-04-09 14:07:13 +0200

    Fixed the test man

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -3,5 +3,5 @@ import { expect, test } from 'vitest'
 
 
 test("find common numbers test", () => {
-  expect(findCommonNumbers(123, 156)).toBe("lol")
+  expect(findCommonNumbers(123, 156)).toBe("12356")
 })
